ecba8b44b03021d2fa0c6f34bffdb3569620a1ee
cirno <846798404@qq.com>
2018-05-04 20:03:54 +0800

    小程序demo

---
+++ client/app.js
@@ -0,0 +1,44 @@
+//app.js
+var qcloud = require('./vendor/wafer2-client-sdk/index')
+var config = require('./config')
+
+App({
+    onLaunch: function () {
+        qcloud.setLoginUrl(config.service.loginUrl)
+        qcloud.login({
+            success(result) {
+                if (result) {
+                    util.showSuccess('登录成功')
+                    that.setData({
+                        userInfo: result,
+                        logged: true
+                    })
+                } else {
+                    // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
+                    qcloud.request({
+                        url: config.service.requestUrl,
+                        login: true,
+                        success(result) {
+                            util.showSuccess('登录成功')
+                            that.setData({
+                                userInfo: result.data.data,
+                                logged: true,
+                            })
+                        },
+
+                        fail(error) {
+                            util.showModel('请求失败', error)
+                            console.log('request fail', error)
+                        }
+                    })
+                }
+            },
+
+            fail(error) {
+                util.showModel('登录失败', error)
+                console.log('登录失败', error)
+            }
+        })
+    }
+
+})
