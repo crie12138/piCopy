ed594eb9cd4223dfaacf51673b5dab5b4856f4af
cirno <846798404@qq.com>
2018-05-05 20:29:59 +0800

    首页设置完成

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -3,44 +3,32 @@ var qcloud = require('../../vendor/wafer2-client-sdk/index')
 var config = require('../../config')
 var util = require('../../utils/util.js')
 
-Page({
+var pageObject = {
     data: {
-        userInfo: {},
-        logged: false,
-        takeSession: false,
-        requestResult: ''
+        userLoading:false,
+        shopkeeperLoading:false,
+        userDisable:false,
+        shopkeeperDisable:false,
     },
-    // 切换是否带有登录态
-    switchRequestMode: function (e) {
-        this.setData({
-            takeSession: e.detail.value
-        })
-        this.doRequest()
-    },
-
-    doRequest: function () {
-        util.showBusy('请求中...')
-        var that = this
-        var options = {
-            url: config.service.requestUrl,
-            login: true,
-            success (result) {
-                util.showSuccess('请求成功完成')
-                console.log('request success', result)
-                that.setData({
-                    requestResult: JSON.stringify(result.data)
-                })
-            },
-            fail (error) {
-                util.showModel('请求失败', error);
-                console.log('request fail', error);
-            }
+    onclick:function(event){
+        var identity=event.currentTarget.dataset.identity
+        console.log(identity)
+        if (identity=="user"){
+            this.setData({
+                userLoading:true,
+                shopkeeperDisable:true
+            })
         }
-        if (this.data.takeSession) {  // 使用 qcloud.request 带登录态登录
-            qcloud.request(options)
-        } else {    // 使用 wx.request 则不带登录态
-            wx.request(options)
+        else
+        {
+            this.setData({
+                userDisable:true,
+                shopkeeperLoading:true
+            })
         }
+        
     },
+  }
 
-})
+
+Page(pageObject)
