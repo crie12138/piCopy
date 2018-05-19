13fc1a3e66867b0680c41917f1693f1b7298e69b
yyg <844753386@qq.com>
2018-05-19 13:48:31 +0800

    updata keeper

--- client/pages/keeper/keeper.js
+++ client/pages/keeper/keeper.js
@@ -5,8 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    
   },
+
   onclick:function(event){
     var navigateUrl=event.currentTarget.dataset.action+"/"+event.currentTarget.dataset.action
     wx.navigateTo({
