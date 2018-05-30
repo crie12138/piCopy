cf6953ad105f22041c748ab03c6053e69d3a62d3
yyg <844753386@qq.com>
2018-05-30 23:37:26 +0800

    去尼玛的

--- client/pages/keeper/shopManage/shop/shop.js
+++ client/pages/keeper/shopManage/shop/shop.js
@@ -1,26 +1,26 @@
 // pages/keeper/shopManage/shop/shop.js
-var config=require("../../../../config")
+var config = require("../../../../config")
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    shop:null,
-    isEdit:false,
+    shop: null,
+    isEdit: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that=this
+    var that = this
     wx.request({
-      url:config.service.shopUrl+"getShop/"+options.shopId,
-      success:function(res){
+      url: config.service.shopUrl + "getShop/" + options.shopId,
+      success: function (res) {
         that.setData({
-          'shop':res.data.row,
-          'isEdit':false
+          'shop': res.data.row,
+          'isEdit': false
         })
       }
     })
@@ -31,96 +31,97 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
-  priceInput:function(event){
+  priceInput: function (event) {
     var reg = /^(([1-9]+)|([0-9]+\.[0-9]{1,2}))$/;
     var result
-    var price=event.detail.value
-    if(reg.test(price)){
-      result="inputCorrect"
+    var price = event.detail.value
+    if (reg.test(price)) {
+      result = "inCorrect"
     }
-    else{
-      result="inputMismatch"
-      price=0
+    else {
+      result = "inMismatch"
+      price = 0
     }
     this.setData({
-      "priceInput":result,
-      "price":price
+      "priceInput": result,
+      "price": price
     })
     console.log(event.detail.value)
-    
+
   },
 
-  submit:function(event){
-    var url=config.service.shopUrl+"shopUpdata"
-    var that=this
-    var form=event.detail.value
-    var options={shopId:form.shopId}
+  submit: function (event) {
+    var url = config.service.shopUrl + "shopUpdata"
+    var that = this
+    var form = event.detail.value
+    console.log(form)
+    console.log("in")
     wx.showModal({
       title: '确认',
       content: '是否更新信息',
-      success: function(res) {
+      success: function (res) {
         if (res.confirm) {
+          console.log("hell")
           wx.request({
-            url:url,
-            data:form,
+            url: url,
+            data: form,
             method: "POST",
-            header: {  
-              "Content-Type": "application/x-www-form-urlencoded"  
+            header: {
+              "Content-Type": "application/x-www-form-urlencoded"
             },
-            success: function(res) {
-              if(res.data.code==0){
+            success: function (res) {
+              console.log(res)
+              if (res.data.code == 0) {
                 //提示修改成功
                 that.setData({
-                  "shop.name":form.name,
-                  "shop.page_price":form.price,
-                  "isEdit":false
+                  "isEdit": false
                 })
               }
-              else{
+              else {
                 //提示更新失败
               }
             }
@@ -130,31 +131,31 @@ Page({
         }
       }
     })
-    
-    
+
+
   },
 
-  editInfo:function(){
+  editInfo: function () {
     this.setData({
-      "isEdit":true
+      "isEdit": true
     })
   },
 
-  shopDelete:function(event){
-    var shopId=event.currentTarget.dataset.shopid
-    var url=config.service.shopUrl+"shopDelete/"+shopId
+  shopDelete: function (event) {
+    var shopId = event.currentTarget.dataset.shopid
+    var url = config.service.shopUrl + "shopDelete/" + shopId
     wx.showModal({
-      title:"删除确认",
-      content:"是否删除商店",
-      success: function(res) {
+      title: "删除确认",
+      content: "是否删除商店",
+      success: function (res) {
         if (res.confirm) {
           wx.request({
-            url:url,
-            success(result){
-              if(result.data.code==0){
+            url: url,
+            success(result) {
+              if (result.data.code == 0) {
                 wx.navigateBack()
               }
-              else{
+              else {
                 //提示删除失败
               }
             }
@@ -164,19 +165,19 @@ Page({
         }
       }
     })
-    
+
     console.log(shopId)
-    
+
   },
-  addPrinter:function(event){
-    var shopId=event.currentTarget.dataset.shopid
+  addPrinter: function (event) {
+    var shopId = event.currentTarget.dataset.shopid
     console.log(shopId)
-    var url="printerManage/printerManage?shopId="+shopId
+    var url = "printerManage/printerManage?shopId=" + shopId
     wx.navigateTo({
-      url:url
+      url: url
     })
   },
-  back:function(){
+  back: function () {
     wx.navigateBack()
   }
 })
