f646decc3fd094d0a962b954ebd06fe32dcd0d62
yyg <844753386@qq.com>
2018-06-06 21:49:04 +0800

    last

--- piCopy-user/dist/main/comtypes/gen/Office.py
+++
@@ -1,3 +0,0 @@
-from comtypes.gen import _2DF8D04C_5BFA_101B_BDE5_00AA0044DE52_0_2_5
-globals().update(_2DF8D04C_5BFA_101B_BDE5_00AA0044DE52_0_2_5.__dict__)
-__name__ = 'comtypes.gen.Office'
---
+++ piCopy-user/派印小程序用户客户端/lib/comtypes/gen/Word.py
@@ -0,0 +1,3 @@
+from comtypes.gen import _00020905_0000_0000_C000_000000000046_0_8_5
+globals().update(_00020905_0000_0000_C000_000000000046_0_8_5.__dict__)
+__name__ = 'comtypes.gen.Word'
